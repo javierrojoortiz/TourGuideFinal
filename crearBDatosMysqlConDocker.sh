acd48af5c4aa5665d5ae4b3267bf6b7ed1baea36
pedrogittaller <pedrogittaller@gmail.com>
2017-07-19 20:06:03 +0200

    cambio menor en script de docker

--- crearBDatosMysqlConDocker.sh
+++ crearBDatosMysqlConDocker.sh
@@ -3,7 +3,7 @@
 sudo apt-get install docker.io
 sudo usermod -aG docker $USER
 docker pull mysql
-cd eclipse-workspace/
+cd /home/java/eclipse-workspace/
 sudo mkdir datos_sql
 docker run --name guidetourdb -v /home/java/eclipse-workspace/datos_sql:/var/lib/mysql -e MYSQL_USER=guidetour_user -e MYSQL_PASSWORD=guidetourabc  -e MYSQL_DATABASE=db_guidetour -e MYSQL_ROOT_PASSWORD=guidetour123 -p 3306:3306 -d mysql:latest
 
