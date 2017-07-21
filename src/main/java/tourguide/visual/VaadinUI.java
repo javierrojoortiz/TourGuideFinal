1c6330f9a37c16e80798d4bcff05a39df4e72262
pedrogittaller <pedrogittaller@gmail.com>
2017-07-21 13:18:29 +0200

    cambios sobre la UI y creacion de formulario de lugares con errores aun

--- src/main/java/tourguide/visual/VaadinUI.java
+++ src/main/java/tourguide/visual/VaadinUI.java
@@ -8,11 +8,13 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.MenuBar.Command;
 import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -27,85 +29,56 @@ import tourguide.logica.LugarRepository;
 @SpringUI
 public class VaadinUI extends UI {
 
-	private final LugarRepository repoLugar;
-
-	private final LugarEditor lugarEditor;
-
-	final Grid<Lugar> grid;
-
-	final TextField filter;
-
-	private final Button addNewBtn;
+//	private final LugarRepository repoLugar;
+//
+//	private final LugarEditor lugarEditor;
+//
+//	final Grid<Lugar> gridLugar;
+//
+//	final TextField filter;
+//
+//	private final Button addNewBtn;
 	
-	final MenuBar menubar;
+	MenuBar menubar;
+	
+	private LugaresForm lugaresform=new LugaresForm();
 	
 
-	@Autowired
-	public VaadinUI(LugarRepository repoLugar, LugarEditor lugarEditor) {
-		this.repoLugar = repoLugar;
-		this.lugarEditor = lugarEditor;
-		this.grid = new Grid<>(Lugar.class);
-		this.menubar = new MenuBar();
-		this.filter = new TextField();
-		this.addNewBtn = new Button("Añadir Lugar", FontAwesome.PLUS);
-	}
+//	@Autowired
+//	public VaadinUI(LugarRepository repoLugar, LugarEditor lugarEditor,LugaresForm lugaresForm) {
+//		this.repoLugar = repoLugar;
+//		this.lugarEditor = lugarEditor;
+//		this.gridLugar = new Grid<>(Lugar.class);
+//		this.menubar = new MenuBar();
+//		this.filter = new TextField();
+//		this.addNewBtn = new Button("Añadir Lugar", FontAwesome.PLUS);
+//		this.lugaresform=lugaresForm;
+//	}
 
 	@Override
 	protected void init(VaadinRequest request) {
 		HorizontalLayout menubarLayout = new HorizontalLayout(menubar);
 		// build layout
-		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-		VerticalLayout mainLayout = new VerticalLayout(menubarLayout,actions, grid, lugarEditor);
+	//	HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+		FormLayout formLayout=new FormLayout();
+		
+		VerticalLayout mainLayout = new VerticalLayout(menubarLayout,formLayout);
+		
+		
+		
+		
 		setContent(mainLayout);
 		
 		menubar.setWidth(100.0f, Unit.PERCENTAGE);
 		menubar.addItem("Hoteles", null, null);
 		menubar.addItem("Restaurantes", null, null);
+		menubar.addItem("Lugares", null, mainLayout.addComponent(lugaresform));//en proceso de cambio
+		
 
-		grid.setWidth(700, Unit.PIXELS);
-		grid.setHeight(500, Unit.PIXELS);
-		grid.setColumns("id", "nombreLugar", "tipo");
-
-		filter.setPlaceholder("Filtrar por tipo");
-
-		// Hook logic to components
-
-		// Replace listing with filtered content when user changes filter
-		filter.setValueChangeMode(ValueChangeMode.LAZY);
-		filter.addValueChangeListener(e -> listCustomers(e.getValue()));
-
-		// Connect selected Customer to editor or hide if none is selected
-		grid.asSingleSelect().addValueChangeListener(e -> {
-			lugarEditor.editLugar(e.getValue());
-		});
-
-		// Instantiate and edit new Customer the new button is clicked
-		addNewBtn.addClickListener(e -> lugarEditor.editLugar(new Lugar("", "")));
-
-		// Listen changes made by the editor, refresh data from backend
-		lugarEditor.setChangeHandler(() -> {
-			lugarEditor.setVisible(false);
-			listCustomers(filter.getValue());
-		});
-
-		// Initialize listing
-		listCustomers(null);
+		
 	}
 	
-    private final Command menuCommand = selectedItem -> selectedItem.getText();
    
-
-	// tag::listLugares[]
-	void listCustomers(String filterText) {
-		if (StringUtils.isEmpty(filterText)) {
-			grid.setItems(repoLugar.findAll());
-		}
-		else {
-			grid.setItems(repoLugar.findByTipoStartsWithIgnoreCase(filterText));
-		}
-	}
-	// end::listLugares[]
-
 }
 
 
